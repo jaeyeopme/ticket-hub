9d9b8b914562f1d0184f5a60575cbb74d8ef8c3d
jaeyeopme <jaeyeopme@gmail.com>
2024-03-18 22:52:19 +0900

    Fix: Validation 관련 상수 정의 수정

--- src/main/kotlin/me/jaeyeop/tickethub/member/adaptor/in/request/CreateMemberRequest.kt
+++ src/main/kotlin/me/jaeyeop/tickethub/member/adaptor/in/request/CreateMemberRequest.kt
@@ -1,14 +1,16 @@
 package me.jaeyeop.tickethub.member.adaptor.`in`.request
 
-import me.jaeyeop.tickethub.support.constant.Message
-import me.jaeyeop.tickethub.support.constant.Regexp
+import me.jaeyeop.tickethub.support.constant.EMAIL_MESSAGE
+import me.jaeyeop.tickethub.support.constant.EMAIL_REGEXP
+import me.jaeyeop.tickethub.support.constant.PHONE_NUMBER_MESSAGE
+import me.jaeyeop.tickethub.support.constant.PHONE_NUMBER_REGEXP
 import jakarta.validation.constraints.NotBlank
 import jakarta.validation.constraints.Pattern
 
 data class CreateMemberRequest(
     @field:Pattern(
-        regexp = Regexp.EMAIL,
-        message = Message.EMAIL
+        regexp = EMAIL_REGEXP,
+        message = EMAIL_MESSAGE
     )
     @field:NotBlank
     val email: String,
@@ -17,8 +19,8 @@ data class CreateMemberRequest(
     @field:NotBlank
     val name: String,
     @field:Pattern(
-        regexp = Regexp.PHONE_NUMBER,
-        message = Message.PHONE_NUMBER
+        regexp = PHONE_NUMBER_REGEXP,
+        message = PHONE_NUMBER_MESSAGE
     )
     @field:NotBlank
     val phoneNumber: String
--- src/main/kotlin/me/jaeyeop/tickethub/support/constant/Validation.kt
+++ src/main/kotlin/me/jaeyeop/tickethub/support/constant/Validation.kt
@@ -1,11 +1,7 @@
 package me.jaeyeop.tickethub.support.constant
 
-object Regexp {
-    const val EMAIL = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}\$"
-    const val PHONE_NUMBER = "^\\d{3}\\d{3,4}\\d{4}\$"
-}
+const val EMAIL_REGEXP = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}\$"
+const val PHONE_NUMBER_REGEXP = "^\\d{3}\\d{3,4}\\d{4}\$"
 
-object Message {
-    const val EMAIL = "유효하지 않은 이메일 형식입니다"
-    const val PHONE_NUMBER = "유효하지 않은 전화번호 형식입니다"
-}
+const val EMAIL_MESSAGE = "유효하지 않은 이메일 형식입니다"
+const val PHONE_NUMBER_MESSAGE = "유효하지 않은 전화번호 형식입니다"
